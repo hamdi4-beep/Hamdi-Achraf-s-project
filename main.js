c481e094dd95a273a8e2328a11b1c7a03a346c10
KHITER Med Achraf <khiter.med7@gmail.com>
2022-02-28 02:00:03 +0100

    Implementing some personel great idees{ progress bar + scroll to top button + smooth scrolling}

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 //Get the button
 var mybutton = document.getElementById("myBtn");
+const pageProgressBar = document.querySelector(".progress-bar")
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -21,3 +26,14 @@ function topFunction() {
     behavior: "smooth",
   });
 }
+
+// adding the progress bar logic
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+});
